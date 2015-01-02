c454513eb83c0c78083160b139deb42052fade2d
Andre Levi <andrelevi09@gmail.com>
2015-01-02 09:43:31 -0800

    change alpha values

--- app/js/controls.js
+++ app/js/controls.js
@@ -84,13 +84,14 @@ function generateSprite() {
     var canvas = document.createElement('canvas');
     canvas.width = 128;
     canvas.height = 128;
+    canvas.globalAlpha = 0.5;
 
     var context = canvas.getContext('2d');
     var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
-    gradient.addColorStop(0, 'rgba(255,255,255,1)');
-    gradient.addColorStop(0.2, 'rgba(0,255,255,1)');
-    gradient.addColorStop(0.4, 'rgba(0,0,64,1)');
-    gradient.addColorStop(1, 'rgba(0,0,0,1)');
+    gradient.addColorStop(0, 'rgba(255,255,255,0.5)');
+    gradient.addColorStop(0.2, 'rgba(0,255,255,0.5)');
+    gradient.addColorStop(0.4, 'rgba(0,0,64,0.5)');
+    gradient.addColorStop(1, 'rgba(0,0,0,0)');
 
     context.fillStyle = gradient;
     context.fillRect(0, 0, canvas.width, canvas.height);
