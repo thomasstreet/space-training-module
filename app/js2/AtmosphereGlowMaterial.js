26e8b4546363fa572695bbc369e21bd23b5aa0ce
Andre Levi <andrelevi09@gmail.com>
2015-04-03 16:16:29 -0700

    clean up dir. remove old js files and assets

--- app/js2/AtmosphereGlowMaterial.js
+++
@@ -1,9 +0,0 @@
-define(function() {
-  return new THREE.ShaderMaterial({
-    vertexShader:   document.getElementById( 'vertexAtmosphereGlow' ).textContent,
-    fragmentShader: document.getElementById( 'fragmentAtmosphereGlow' ).textContent,
-    side: THREE.BackSide,
-    blending: THREE.AdditiveBlending,
-    transparent: true
-  });
-});
