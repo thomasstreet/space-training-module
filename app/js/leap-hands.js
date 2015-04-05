a555a51cc227291fd696f4177fa7114fd162bdcf
Andre Levi <andrelevi09@gmail.com>
2015-04-05 09:18:33 -0700

    move Hand class into separate file

--- app/js/leap-hands.js
+++ app/js/leap-hands.js
@@ -1,107 +1,6 @@
 require('traceur/bin/traceur-runtime');
 
-class Hand {
-  constructor(type, group) {
-    this.type = type;
-    this.isVisible = false;
-    this.objectBeingHeld = null;
-
-    var outer = new THREE.Mesh(
-      new THREE.TorusGeometry(25, 0.5, 32, 32),
-      new THREE.MeshBasicMaterial({ color: 0xffffff }) 
-    );
-    var inner = new THREE.Mesh(
-      new THREE.TorusGeometry(20, 0.5, 32, 32),
-      new THREE.MeshBasicMaterial({ color: 0xffffff }) 
-    );
-    inner.position.z = -3;
-
-    this.palm = new THREE.Group();
-    this.palm.add(outer);
-    this.palm.add(inner);
-    this.palm.position.z = 200;
-    this.palm.posSequence = [];
-    this.palm.normalSequence = [];
-
-    // Hide until active
-    hide(this.palm);
-    group.add(this.palm);
-
-    this.fingerTips = [];
-    this.velocity = null;
-
-    var geometry = new THREE.CylinderGeometry(10, 10, 1, 32, 32);
-    var material = new THREE.MeshBasicMaterial();
-    for (var i = 0; i < 5; i++) {
-      var sphere = new THREE.Mesh(geometry, material);
-      this.fingerTips.push(sphere);
-      hide(sphere);
-      group.add(sphere);
-    }
-  }
-
-  getRollingAverage(array, newPos) {
-    var maxLength = 10;
-    array.push(newPos);
-    if (array.length >= maxLength) {
-      array.shift();
-    }
-    var len = array.length;
-    var x = 0, y = 0, z = 0;
-    for (var i = 0; i < len; i++) {
-      var pos = array[i];
-      x += pos[0];
-      y += pos[1];
-      z += pos[2];
-    }
-    var out = [
-      x / len,
-      y / len,
-      z / len
-    ];
-    return out;
-  }
-
-  showHand(data) {
-    this.isVisible = true;
-
-    data.fingers.forEach((finger, i) => {
-      show(this.fingerTips[i]);
-    });
-
-    show(this.palm);
-  }
-
-  hideHand(data) {
-    this.isVisible = false;
-
-    data.fingers.forEach((finger, i) => {
-      hide(this.fingerTips[i]);
-    });
-
-    hide(this.palm);
-  }
-
-  isHoldingThisObject(object) {
-    return this.objectBeingHeld === object;
-  }
-
-  isHoldingAnyObject() {
-    return !!this.objectBeingHeld;
-  }
-
-  stopHoldingObject() {
-    this.objectBeingHeld = null;
-  }
-
-  holdObject(object) {
-    this.objectBeingHeld = object; 
-  }
-
-  getHeldObject() {
-    return this.objectBeingHeld;
-  }
-}
+var Hand = require("./classes/Hand");
 
 var yOffset = -300;
 
@@ -137,18 +36,6 @@ var loop = Leap.loop({background: true}, {
 })
 // Provides handFound/handLost events.
 .use('handEntry');
-
-function show(mesh) {
-  mesh.traverse(function(child) {
-    child.visible = true;
-  });
-}
-
-function hide(mesh) {
-  mesh.traverse(function(child) {
-    child.visible = false;
-  });
-}
 
 module.exports = {
   loop: loop,
